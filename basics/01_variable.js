8c0d32fa134a6b6c24eba73425bd1c46f0533da4
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-11-29 12:25:32 +0000

    variable declaration

---
+++ basics/01_variable.js
@@ -0,0 +1,15 @@
+const myAccount=12345678;
+let myEamil="abc@gmail.com ";
+var myPassword="12345";
+otherAccount=245676543;
+let myPlace;
+
+// myAccount=34;
+
+// The "Assignment to constant variable" error occurs when trying to reassign or redeclare a variable declared using the const keyword. When a variable is declared using const , it cannot be reassigned or redeclared.
+
+console.log(myAccount);
+console.table([myAccount,myEamil,myPassword,otherAccount,myPlace]);
+
+
+// Variable declaration:const, let, var when we use const can’t change value but we can do this with let and var.var doesn’t follow scope that why we should always use let or const most of the people prefer let.if we declare any variable and we don’t assign any value after consoling we see this is undefined because when you don’t assign any variable js save this variable value as undefined .
