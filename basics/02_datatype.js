1981d09de7e1c3db930382ec6d5d2970dd292ffe
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-11-29 13:22:54 +0000

    datatype

---
+++ basics/02_datatype.js
@@ -0,0 +1,18 @@
+let name="mejbha";
+let age=34;
+let isHeComing=true;
+
+
+// type of datatype
+// number=> 2 to power 53
+// string=>""
+// boolean=>true/false 
+// undefined=>
+// null=>standlone value 
+// symbol=>unique 
+// this all are premitive data type 
+
+console.log(typeof "mejbha");
+console.log(typeof mejbha);
+
+
