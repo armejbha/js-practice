ffb53719360644dce8f56a96f6be913791987439
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-11-29 15:55:02 +0000

    comparision

---
+++ basics/04_comparision.js
@@ -0,0 +1,10 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+
+console.log("12" >13);
+
+// null comparision == and > <= => are different.comparision treat null like 0.
+// within undefined comparision is always false 
+
+// == just check value and === check value and data type 
