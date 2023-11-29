37dc5cc3cfc2ff34d0ec47e8b853f2a48908d193
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-11-29 23:29:27 +0000

    string

---
+++ basics/05_string.js
@@ -0,0 +1,9 @@
+const otherName='mejbha';
+const myAge=34;
+
+// console.log(`my name is ${otherName} and my age is${myAge}`);
+
+const myName=new String('mejbha');
+console.log(myName.indexOf('b'));
+
+// string and newString are not same have huge difference one is primitive string and other is string object.
---
+++ basics/datatype-summary.js
@@ -0,0 +1,3 @@
+// ++++++++++++++++++++++++++++++++++ Memory +++++++++++++++++++++++++++++++++++++++++++++
+// stack (primitive)  and Heap (non primitive)
+// stack always recive a copy and heap recive a reference 
