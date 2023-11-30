08ad2e88516baae0dadf2017aae5a054b229d227
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-11-30 00:18:00 +0000

    num and math

---
+++ basics/06-num_math.js
@@ -0,0 +1,20 @@
+const score=400;
+
+const number=new Number(400);
+
+// console.log(score)
+// console.log(typeof);
+
+const otherNumber=1.4999
+// console.log(otherNumber.toPrecision(2))
+// toFixed and toPrecious both return string but toFixed define value and toPrecious define length
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+
+console.log((Math.random()*10)+1);
