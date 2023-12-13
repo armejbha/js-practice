91280faaec8a2e44f4dcf9ca04c77d0102a8af1b
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2023-12-13 10:52:45 +0000

    loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,25 @@
+
+// const array=["mejbha","Shalil","iftu","rahim"];
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     if(index==2){
+//         continue
+//     }
+//     console.log(element)
+    
+// }
+// let index=0;
+
+// while (index<=10) {
+//     console.log(`The value is :${index}`)
+//     index = index + 2; 
+// }
+let index=0
+do {
+    console.log(`The value is:${index}`)
+    index =index+1
+} while (index<=10);
+
+// loop is working follow some statement like initialize condition check finally increment for ,while and dowhile all loop working follow statement.
